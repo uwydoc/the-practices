bc0600795cb894346233e3813e2164e6c5385234
uwydoc <uwydoc@gmail.com>
2015-04-23 12:48:07 +0800

    Add scripts to ease the build process

---
+++ boost/build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+[ -z "$1" ] && echo "Usage: build.sh file [other_file...]" && exit 0
+
+set -e
+
+CC=g++
+BOOST_ROOT=/d/Downloads/boost-build-1_55_0
+BOOST_LIBRARYDIR=/d/Downloads/boost-build-1_55_0/mingw/stage/lib
+
+# compile
+ls $@ | while read filename; do
+    outfile=${filename%.*};
+    rm -f runtime/$outfile;
+    $CC $CFLAGS $CXXFLAGS -I "$BOOST_ROOT" -L "$BOOST_LIBRARYDIR" -o runtime/$outfile $filename $LFLAGS || break;
+done
+
+# run
+ls $@ | while read filename; do
+    outfile=${filename%.*};
+    echo "== $outfile ==";
+    ./runtime/$outfile;
+    echo "";
+done
